169afd1f6d8f43f5c9be13a594530856c014eb1e
mahimerror <mahimislam46@gmail.com>
2025-08-26 16:21:59 +0600

    have a need finantial dialog responsive

--- src/components/dialog/HaveNeedDialogFinancial.jsx
+++ src/components/dialog/HaveNeedDialogFinancial.jsx
@@ -13,15 +13,15 @@ const HaveNeedDialogFinancial = ({ open, onOpenChange }) => {
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent
         showCloseButton={false}
-        className="border-0 rounded-3xl w-full !max-w-[800px] bg-[#F9F5F0]"
+        className="border-0 rounded-xl sm:rounded-2xl xl:rounded-3xl w-full !max-w-[95vw] sm:!max-w-[90vw] lg:!max-w-[800px] bg-[#F9F5F0]"
       >
         <DialogHeader>
           <DialogDescription
             className={
-              "flex flex-col items-center gap-6 text-center p-12 text-lg"
+              "flex flex-col items-center gap-4 sm:gap-6 text-center sm:p-8 md:p-12 text-sm sm:text-base md:text-lg"
             }
           >
-            <HeartIcon />
+            <HeartIcon className="size-14 sm:size-16 lg:size-auto" />
             <div className="">
               <p>Thank you for trusting us with your story.</p>
               <p>
@@ -31,7 +31,7 @@ const HaveNeedDialogFinancial = ({ open, onOpenChange }) => {
               </p>
             </div>
             <p>You are not alone. You are seen. You are safe here.</p>
-            <div className="pt-4">
+            <div className="lg:pt-4">
               <Button className="rounded-full px-20" asChild>
                 <Link to="/">Back to home</Link>
               </Button>
