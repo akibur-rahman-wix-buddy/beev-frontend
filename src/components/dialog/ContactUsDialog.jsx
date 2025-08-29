96519429d70ab4b82653c080131167002457bf4d
mahimerror <mahimislam46@gmail.com>
2025-08-29 10:58:42 +0600

    responsive ContactUsDialog

--- src/components/dialog/ContactUsDialog.jsx
+++ src/components/dialog/ContactUsDialog.jsx
@@ -13,23 +13,23 @@ const ContactUsDialog = ({ open, onOpenChange }) => {
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent
         showCloseButton={false}
-        className="border-0 rounded-4xl w-full !max-w-[800px]"
+        className="border-0 rounded-xl sm:rounded-2xl xl:rounded-3xl w-full !max-w-[95vw] sm:!max-w-[90vw] lg:!max-w-[800px] bg-[#F9F5F0]"
       >
         <DialogHeader>
           <DialogDescription
             className={
-              "flex flex-col items-center gap-6 text-center p-12 text-lg font-medium"
+              "flex flex-col items-center gap-4 sm:gap-6 text-center sm:p-8 md:p-12 text-sm sm:text-base md:text-lg"
             }
           >
-            <HeartIcon />
+            <HeartIcon className="size-14 sm:size-16 lg:size-auto" />
             <p>
               Thank you for contacting One Heart Society. We appreciate you
               taking the time to reach out. Our team will respond as soon as
               we're able.
             </p>
-            <div className="pt-4">
-              <Button asChild>
-                <Link to="/">Go Home</Link>
+            <div className="lg:pt-4">
+              <Button className="rounded-full px-20" asChild>
+                <Link to="/">Back to home</Link>
               </Button>
             </div>
           </DialogDescription>
